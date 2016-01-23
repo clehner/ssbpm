67d6055ff9935b8ab1faf62075bc7b6857645a50
Charles Lehner <cel@celehner.com>
2016-01-23 17:23:51 -0500

    Expand usage message. Add version message

--- bin.js
+++ bin.js
@@ -4,6 +4,7 @@ var argv = require('minimist')(process.argv.slice(2), {
   alias: {
     save: 'S',
     force: 'f',
+    version: 'v',
     help: 'h'
   }
 })
@@ -21,13 +22,34 @@ function createSsbClient(cb) {
   }, cb)
 }
 
-function usage (status) {
-  console.log('Install a package: ssbpm install {hash...}')
-  console.log('Publish a package: ssbpm publish [directory]')
+function usage(status) {
+  console.log([
+    '',
+    '  ssbpm <command> [options]',
+    '',
+    '  Options:',
+    '',
+    '    -h, --help     Output this message',
+    '    -v, --version  Output version information',
+    '    -s, --save     Update package.json after the operation',
+    '    -f, --force    Ignore warnings',
+    '',
+    '  Commands:',
+    '',
+    '    install <hash>       Install a package',
+    '    publish [directory]  Publish a package',
+    ''].join('\n'))
   process.exit(status)
 }
 
-if (argv._.length == 0 || argv.help)
+function version() {
+  console.log(require('./package.json').version)
+  process.exit(0)
+}
+
+if (argv.version)
+  version()
+else if (argv._.length == 0 || argv.help)
   usage(0)
 else if (argv._[0] == 'install')
   install(argv._.slice(1), argv)
